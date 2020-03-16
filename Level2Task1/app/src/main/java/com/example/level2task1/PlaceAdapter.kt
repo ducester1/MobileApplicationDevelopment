35807d5d448036c69d4166d2f517f35c51737111
Scott <schepers.scott@hotmail.com>
2020-03-16 11:41:52 +0100

    Added: Level2Task1

---
+++ Level2Task1/app/src/main/java/com/example/level2task1/PlaceAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.level2task1
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_place.view.*
+
+public class PlaceAdapter(private val places: List<Place>) : RecyclerView.Adapter<PlaceAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(place : Place) {
+            itemView.ivPlace.setImageResource(place.imageResID)
+            itemView.tvPlace.text = place.name
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_place, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return places.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(places[position])
+    }
+}
